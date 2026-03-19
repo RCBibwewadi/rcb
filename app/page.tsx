9fc5c228576c18b5ed3b0afa4f410e1080df8cf6
face-usp <uspface@gmail.com>
2026-03-19 22:03:26 +0530

    Revert "feat: game feat"
    This reverts commit e54616a8dbe8119941d56a00ae4714454ef77778.

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,12 @@ import Hero from "@/components/Hero";
 import JoinUsSection from "@/components/JoinUs";
 import Navbar from "@/components/Navbar";
 import Projects from "@/components/Projects";
-import GameBanner from "@/components/GameBanner";
 
 export default function Home() {
   return (
     <main className="flex flex-col">
       <Navbar />
       <Hero />
-      <GameBanner />
       <About />
       <Board />
       <Projects />
